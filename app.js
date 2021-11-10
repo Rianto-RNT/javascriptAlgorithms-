2efea2e3d075cadb9cbb97f5425805872b293431
Rianto <rianto.rnt@gmail.com>
2021-11-10 20:21:31 +0800

    finabocci big O

--- app.js
+++ app.js
@@ -16,12 +16,17 @@
 // console.log(sumNumbers([1, 3, 10, 50]));
 
 function fib(n) {
-  const numbers = [1, 1];
-  for (let i = 2; 1 < n + 1; i++) {
-    numbers.push(numbers[i - 2] + numbers[i - 1]);
+  const numbers = [1, 1]; // 1
+  let counts = 0;
+  for (let i = 2; 1 < n + 1; i++) {  // 1
+    console.log('Running'); // n - 1
+    numbers.push(numbers[i - 2] + numbers[i - 1]); // n - 1
   }
-  return numbers[n];
+  return numbers[n]; // 1
 }
 
+// T = 1 + 1 + 1 + 2 * (n - 1) = 3 + 2n - 2 = 1 + 2n = 2n = n
+// => O(n) => Linier Time Complexity
+
 console.log(fib(4));
-console.log(fib(5));
+// console.log(fib(5));
