43b360163b589d777160910ed7bb34fbdbac08cd
Rianto <rianto.rnt@gmail.com>
2021-11-10 20:55:55 +0800

    math isPrine improve

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 function isPrime(number) {
-  for (let i = 2; i < number; i++) {
+  for (let i = 2; i < Math.sqrt(number); i++) {
+    // 1
+    console.log("Running");
     if (number % i === 0) {
-      return false;
+      // n
+      return false; // 1
     }
   }
-  return true;
+  return true; // 1
 }
 
-console.log(isPrime(5));
-console.log(isPrime(9));
+// best case: number = 1 OR number = 2 O(1)
+// Average case: number = 27,277 => O(n) (Improved: O(sqrt(n)))
+
+console.log(isPrime(27277));
+// console.log(isPrime(5));
+// console.log(isPrime(9));
