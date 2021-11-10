34bdc583ed3ab2a521281783c26d93cd2a24fc56
Rianto <rianto.rnt@gmail.com>
2021-11-10 20:12:52 +0800

    finabocci solution

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
+// // function sumNumbers(numbers) {
+// //   return numbers[0] + numbers[1] + numbers[2];
+// // }
+
+// // console.log(sumNumbers([1, 3, 10]));
+
 // function sumNumbers(numbers) {
-//   return numbers[0] + numbers[1] + numbers[2];
+//   let result = 0;
+//   for (const number of numbers) {
+
+//     result += number;
+//   }
+//   return result;
 // }
 
-// console.log(sumNumbers([1, 3, 10]));
+// console.log(sumNumbers([1, 3, 10, 50]));
 
-function sumNumbers(numbers) {
-  let result = 0;
-  for (const number of numbers) {
-    result += number;
+function fib(n) {
+  const numbers = [1, 1];
+  for (let i = 2; 1 < n + 1; i++) {
+    numbers.push(numbers[i - 2] + numbers[i - 1]);
   }
-  return result;
+  return numbers[n];
 }
 
-console.log(sumNumbers([1, 3, 10, 50]));
+console.log(fib(4));
+console.log(fib(5));
